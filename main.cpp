24791355814e0a33ceb86681ea30060a4fc229d8
DanmerZ <oleh.pomazan@gmail.com>
2015-11-13 10:21:57 +0300

    final comment

--- main.cpp
+++ main.cpp
@@ -8,15 +8,15 @@ using namespace std;
 int main()
 {
     students();
-    twolists();
+//    twolists();
 
-    problem_1();
-    problem_2();
-    problem_3();
-    problem_4();
-    problem_5();
-    problem_6();
-    problem_7();
+//    problem_1();
+//    problem_2();
+//    problem_3();
+//    problem_4();
+//    problem_5();
+//    problem_6();
+//    problem_7();
 
 
     return 0;
